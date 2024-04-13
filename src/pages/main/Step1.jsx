90123e3dedb5b7bc5fa77f5ccc333e7e3f458341
dd-stack <117844745+dd-stack@users.noreply.github.com>
2024-04-13 22:42:58 +0900

    1단계 페이지 디자인 완성

--- src/pages/main/Step1.jsx
+++ src/pages/main/Step1.jsx
@@ -1,7 +1,50 @@
+import styled from "styled-components";
+
+const QuestionContainer = styled.div`
+  width: 320px;
+  height: 140px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.1rem;
+  font-weight: 600;
+  background-color: #fff;
+  border: #072e915e 1px solid;
+  border-radius: 10px;
+  padding-bottom: 10px;
+  > span {
+    color: #072e91;
+    font-size: 1rem;
+    margin-bottom: 10px;
+  }
+`;
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px 0 60px 0;
+`;
+
+const AnswerButton = styled.button`
+  font-size: 1.1rem;
+  font-weight: 400;
+  margin-top: 10px;
+`;
+
 export default function Step1() {
   return (
-    <div>
-      <h1>1</h1>
-    </div>
+    <>
+      <QuestionContainer>
+        <span>(1단계)</span>
+        수출하고자 하는 사업은 무엇인가요?
+      </QuestionContainer>
+      <ButtonsWrapper>
+        <AnswerButton>EPC 수출</AnswerButton>
+        <AnswerButton>산업 설비 수출</AnswerButton>
+        <AnswerButton>단순 상품 및 제품 수출</AnswerButton>
+      </ButtonsWrapper>
+    </>
   );
 }
