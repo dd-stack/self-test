1a8ab2e62dd907010a4958ecb12cef9558b8357b
dd-stack <117844745+dd-stack@users.noreply.github.com>
2024-04-13 21:48:41 +0900

    헤더 완성

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,7 +1,50 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { BsArrowLeft, BsArrowClockwise } from "react-icons/bs";
+
+const HeaderContainer = styled.div`
+  width: 320px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+`;
+
+const MenuButton = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 1.2rem;
+  font-weight: 700;
+  color: #072e915e;
+  text-shadow: -1px 0px #fff, 0px 1px #fff, 1px 0px #fff, 0px -1px #fff;
+  cursor: pointer;
+  > svg {
+    color: #fff;
+    margin-right: 5px;
+  }
+`;
+
 export default function Header() {
+  const navigate = useNavigate();
+
+  function handleBackClick() {
+    navigate(-1);
+  }
+
+  function handleHomeClick() {
+    navigate("/");
+  }
+
   return (
-    <div>
-      <h1>헤더</h1>
-    </div>
+    <HeaderContainer>
+      <MenuButton onClick={handleBackClick}>
+        <BsArrowLeft />
+        뒤로가기
+      </MenuButton>
+      <MenuButton onClick={handleHomeClick}>
+        <BsArrowClockwise />
+        처음으로
+      </MenuButton>
+    </HeaderContainer>
   );
 }
