d89146aadd5c1970e85a90f049699861eba00c21
dd-stack <117844745+dd-stack@users.noreply.github.com>
2024-04-13 20:52:54 +0900

    타이틀 화면 디자인

--- src/pages/Title.jsx
+++ src/pages/Title.jsx
@@ -1,9 +1,32 @@
+import styled from "styled-components";
+
+import logo from "../assets/logo.png";
+
+const EntireContainer = styled.div`
+  margin-top: 60pt;
+`;
+
+const TitleText = styled.h1`
+  margin-top: 10pt;
+  font-size: 2.2rem;
+  font-weight: 700;
+  line-height: 1.7;
+`;
+
+const StartButton = styled.button`
+  margin-top: 30pt;
+`;
+
 export default function Title() {
   return (
-    <div>
-      <h1>시장 개척 지원 사업</h1>
-      <h1>자가 진단 하기</h1>
-      <button>START</button>
-    </div>
+    <EntireContainer>
+      <img src={logo} width="150px" alt="KOPIA logo" />
+      <TitleText>
+        시장 개척 지원 사업
+        <br />
+        자가 진단
+      </TitleText>
+      <StartButton>시작하기</StartButton>
+    </EntireContainer>
   );
 }
